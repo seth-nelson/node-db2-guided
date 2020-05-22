651845b5566491d1bd68914ba2d226987ab5906c
Seth Nelson <seth.nelson@outlook.com>
2020-05-21 19:42:07 -0700

    added files for project

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+5// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/produce.db3'
+    },
+
+    useNullAsDefault: true
+  }
+
+};
